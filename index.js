0391f7e970a88c4b76b70797dfdea17b6dadf138
eman ayman <emanayman657@yahoo.com>
2019-08-22 11:38:28 +0200

    intial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+function eman()
+{
+    console.log("yup");
+    var container =document.getElementById('main');
+    var image =document.createElement=('img');
+    image.setAttribute("src",'https://besthqwallpapers.com/Uploads/5-4-2019/86249/thumb-bouquet-of-tulips-pink-tulips-postcard-spring-flowers-tulips-on-a-pink-background');
+    container.appendChild(image);
+};
